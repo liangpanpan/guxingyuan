fe89699d905a1daff799d87608a678375ad95995
panpanliang <liangpan8224091@163.com>
2025-01-12 17:01:37 +0800

    添加静态变量初始化测试类

---
+++ test-project/src/main/java/com/pp/test/PrintTest.java
@@ -0,0 +1,22 @@
+package com.pp.test;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2025/1/12       create this file
+ * </pre>
+ */
+public class PrintTest {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+//        Class<?> aClass = Class.forName("com.pp.test.StaticTest");
+
+        System.out.println(StaticTest.A);
+        System.out.println("111222333");
+        System.out.println(StaticTest.NAME);
+
+    }
+}
---
+++ test-project/src/main/java/com/pp/test/StaticTest.java
@@ -0,0 +1,21 @@
+package com.pp.test;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2025/1/12       create this file
+ * </pre>
+ */
+public class StaticTest {
+
+    public static final String A = "AAAA";
+
+    public static String NAME = "123";
+
+    static {
+        System.out.println("StaticTest init");
+        NAME = "111";
+    }
+}
